3ce5c643bcdc2e414827831035893f73ecfe3d26
Amirjon Karimov <amirjonkarimoff@gmail.com>
2024-11-11 22:38:40 +0500

    deploy

--- bot/keyboards/inline/boglanish_button.py
+++ bot/keyboards/inline/boglanish_button.py
@@ -6,6 +6,6 @@ check.insert(InlineKeyboardButton(text="❌Yo'q", callback_data='Yoq'))
 
 startkeyboard = InlineKeyboardMarkup(row_width=2)
 startkeyboard.insert(InlineKeyboardButton(text="✅Fiverr",url='fiverr.com/amirjonkarimov'))
-startkeyboard.insert(InlineKeyboardButton(text="📹You tube",url='https://www.youtube.com/@AmirjonKarimovBlog'))
-startkeyboard.insert(InlineKeyboardButton(text="🌐Instagram",url='instagram.com/amirjonkarimov.blog'))
 startkeyboard.insert(InlineKeyboardButton(text="🔰Linkedin",url='https://www.linkedin.com/in/amirjon-karimov'))
+startkeyboard.insert(InlineKeyboardButton(text="🌐Instagram",url='instagram.com/amirjonkarimov.blog'))
+startkeyboard.insert(InlineKeyboardButton(text="📹You tube",url='https://www.youtube.com/@AmirjonKarimovBlog'))
